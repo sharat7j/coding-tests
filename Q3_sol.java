0b8cf78b22d3e3e71d2aa552eb0cd052b3d981c2
sharat7j <sharsmail@gmail.com>
2016-06-12 15:53:33 -0700

    Create Q3_sol.java

---
+++ Q3_sol.java
@@ -0,0 +1,71 @@
+public static int[] searchRange(int[] nums, int target) {
+		int end = nums.length;
+		int start = 0;
+		int startPos = -1;
+		int endPos = -1;
+
+		while (start <= end) {
+			int mid = (start + end) / 2;
+
+			if (target == nums[mid]) {
+				startPos = mid;
+				endPos = mid;
+
+				int tmp = findVal(start, mid, nums, target, mid, "lower");
+				if (tmp != -1 && tmp < startPos)
+					startPos = tmp;
+				tmp = findVal(mid, end, nums, target, mid, "higher");
+				if (tmp != -1 && endPos < tmp)
+					endPos = tmp;
+				break;
+
+			} else if (target < nums[mid]) {
+
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+
+		}
+		int[] targetRange = new int[2];
+		targetRange[0] = startPos;
+		targetRange[1] = endPos;
+		return targetRange;
+	}
+
+	public static int findVal(int start, int end, int[] nums, int target, int pos, String dir) {
+
+		if (start > end) {
+			return pos;
+		}
+		int mid = (end + start) / 2;
+
+		if (mid < nums.length) {
+			if (target == nums[mid]) {
+				pos = mid;
+				if (dir.equals("lower")) {
+					int tmp = findVal(start, mid - 1, nums, target, pos, dir);
+					if (tmp < pos)
+						pos = tmp;
+				} else {
+					int x = findVal(mid + 1, end, nums, target, pos, dir);
+					if (x > pos)
+						pos = x;
+				}
+				return pos;
+			} else if (nums[mid] < target && dir.equals("lower")) {
+				int tmp = findVal(mid + 1, end, nums, target, pos, dir);
+				if (tmp < pos)
+					pos = tmp ;
+
+			} else if (nums[mid] > target && dir.equals("higher")) {
+
+				int tmp = findVal(start, mid - 1, nums, target, pos, dir);
+				if (tmp > pos)
+					pos = tmp;
+			}
+		}
+		return pos;
+
+	}
+
