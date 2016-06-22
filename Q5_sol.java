dda02a1da6f8e5ccf428abbc66acfb4e442a5dcd
sharat7j <sharsmail@gmail.com>
2016-06-21 20:23:45 -0700

    Create Q5_sol.java

---
+++ Q5_sol.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+import java.util.PriorityQueue;
+
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> queue=new     PriorityQueue<Integer>();
+        for(int i=0;i<lists.length;i++){
+            ListNode list=lists[i];
+            while(list!=null){
+                queue.add(list.val);
+                list=list.next;
+            }
+        }
+        ListNode result=null;
+        ListNode start=null;
+        if(!queue.isEmpty()){
+                   result=new ListNode(queue.remove());
+                   start=result;
+        }
+        while(!queue.isEmpty()){
+            ListNode tmp=new ListNode(queue.remove());
+            result.next=tmp;
+            result=tmp;
+            
+            
+            
+            
+        }
+        return start;
+    }
+}
