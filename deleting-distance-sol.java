5fa21ac7c614f2b2bb26859fdbfbbed832a671d6
sharat7j <sharsmail@gmail.com>
2018-05-03 00:10:59 -0700

    Create deleting-distance-sol.java

---
+++ deleting-distance-sol.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+ 
+  static int lcs(String s1, String s2){
+    //String lcsString="";
+    //code for lcs here 
+    int len1=s1.length();
+    int len2=s2.length();
+    int[][] dp=new int[len1+1][len2+1];
+    for(int i=0;i<=len1;i++){
+      for(int j=0;j<=len2;j++){
+          if(i==0 || j==0)
+          {
+            dp[i][j]=0;
+          }
+          if(s1.charAt(i-1)==s2.charAt(j-1)){
+            dp[i][j]=dp[i-1][j-1]+1;
+          }else{
+            dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+          }
+      }
+    }
+    
+    
+    
+    return dp[len1][len2];
+  }
+  
+  static int deletionDistance(String str1, String str2) {
+    // s1= teah s2=hit
+    //s1 = teah s2=ateh
+    int lcs=lcs(str1,str2);
+    return (str1.length()-lcs))+(str2.length()-lcs;
+      
+    
+    
+   
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
